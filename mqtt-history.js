5b0164f291d9205ea4861cb6ffac566a047f13e3
thunder <thunder@debianserver.thunderace.fr>
2015-01-15 16:36:31 +0100

    timestamp option added

--- mqtt-history.js
+++ mqtt-history.js
@@ -9,28 +9,39 @@ program.version('0.0.1')
   .option('-p, --redisport [redis port]', 'redis DDB port number [6379]', 6379)
   .option('-m, --mqtthostname [mqtt hostname]', 'MQTT broker hostame or ip', null)
   .option('-q, --mqttport [mqtt port]', 'MQTT broker port number [1883]', 1883)
+  .option('-t, --timestamp', 'Add timestamp to redis saved payload', false)
+  
 	.parse(process.argv);
 
 //------------ DEBUG
-var debug_on = false;
+var debug_on = true;
 function debug() {
   if (debug_on) console.log.apply(this, arguments);
 }
 var nop = function() {};
-console.log(program.mqtthostname);
+
 
 redisClient = redis.createClient(program.redisport, program.redishostname);
-mqttClient = mqtt.createClient(program.mqtthostname, program.mqttport);
+mqttClient = mqtt.createClient(program.mqtthostname, program.mqttport, {clientId: 'mqtt-history'});
 mqttClient.on("connect", function() {
   debug ('MQTT connected');
-  mqttClient.on('message', function(topic, payload) {  
+  mqttClient.on('message', function(topic, payload, data) {  
+    console.log(data);
     // replace all / by :
     var redisTopic = topic.replace(/\//g, ':');
     if (redisTopic[0] == ':') {
       redisTopic = redisTopic.slice(1);
     }
     debug('lpush ' + redisTopic + ' to redis');
-    redisClient.lpush('MQTTHistory:' + redisTopic, payload);
+    if (program.timestamp) {
+      var history = {
+        timestamp:  new Date(),
+        value : payload
+      };
+      redisClient.lpush('MQTTHistory3:' + redisTopic, JSON.stringify(history));
+    } else {
+      redisClient.lpush('MQTTHistory3:' + redisTopic, payload);
+    }
   });
-  mqttClient.subscribe('/#');
+  mqttClient.subscribe('/#', {qos: 2}); // only once
 });
