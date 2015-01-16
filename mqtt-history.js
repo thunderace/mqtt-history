82c0a01184fdf05846a6d910bcdf12af4c6cce66
thunder <thunder@debianserver.thunderace.fr>
2015-01-16 09:51:37 +0100

    back to old mqtt version - new options (skip null payload - set redis basepath - add timestamp) - store in JSON format

--- mqtt-history.js
+++ mqtt-history.js
@@ -4,43 +4,93 @@ var program = require('commander');
 var redis = require('redis');
 var mqtt = require('mqtt');
 
-program.version('0.0.1')
+program.version('0.0.3')
   .option('-h, --redishostname [redis hostname]', 'redis DDB hostame or ip', null)
   .option('-p, --redisport [redis port]', 'redis DDB port number [6379]', 6379)
   .option('-m, --mqtthostname [mqtt hostname]', 'MQTT broker hostame or ip', null)
   .option('-q, --mqttport [mqtt port]', 'MQTT broker port number [1883]', 1883)
-  .option('-t, --timestamp', 'Add timestamp to redis saved payload', false)
-  
+  .option('-t, --timestamp', 'Add timestamp to redis saved payload')
+  .option('-r, --redisprefix [mqtt port]', 'Redis prefix', 'mqtt-history')
+  .option('-s, --skipnullpayload', 'Do not save null payload')
 	.parse(process.argv);
 
+
+program.timestamp = program.timestamp || false;
+program.skipnullpayload = program.skipnullpayload || false;
 //------------ DEBUG
-var debug_on = false;
+var debug_on = true;
 function debug() {
   if (debug_on) console.log.apply(this, arguments);
 }
-var nop = function() {};
 
+var nop = function() {};
 
 redisClient = redis.createClient(program.redisport, program.redishostname);
 mqttClient = mqtt.createClient(program.mqtthostname, program.mqttport, {clientId: 'mqtt-history'});
+
 mqttClient.on("connect", function() {
   debug ('MQTT connected');
-  mqttClient.on('message', function(topic, payload, data) {  
-    // replace all / by :
-    var redisTopic = topic.replace(/\//g, ':');
-    if (redisTopic[0] == ':') {
-      redisTopic = redisTopic.slice(1);
-    }
-    debug('lpush ' + redisTopic + ' to redis');
-    if (program.timestamp) {
-      var history = {
-        timestamp:  new Date(),
-        value : payload
-      };
-      redisClient.lpush('mqtt-history:' + redisTopic, JSON.stringify(history));
-    } else {
-      redisClient.lpush('mqtt-history:' + redisTopic, payload);
-    }
-  });
+  mqttClient.on('message', onMessage);
   mqttClient.subscribe('/#', {qos: 2}); // only once
 });
+
+
+function onMessage(topic, payload, packet) {
+  if (program.skipnullpayload == true && !payload) {
+    return;
+  }
+  // replace all / by :
+  var redisTopic = topic.replace(/\//g, ':');
+  if (redisTopic[0] == ':') {
+    redisTopic = redisTopic.slice(1);
+  }
+  if (packet.retain == true) {
+    //  try to avoid duplicate topic saved 
+    //  get the last value stored for this topic
+    //  ? this value is with timestamp
+    //    yes : extract data
+    //  ? compare payload to current payload
+    //    same      :  abort
+    //    different : save
+    redisClient.lindex(program.redisprefix + ':' + redisTopic, 0, function(err, reply) {
+      if (reply == null) {
+        debug('reply null for ' + program.redisprefix + ':' + redisTopic);
+        saveToRedis(redisTopic, payload);
+        return;
+      } 
+      var value;
+      try {
+        value = JSON.parse(reply);
+        if (value.timestamp && value.data) {
+          if (value.data != payload) {
+            saveToRedis(redisTopic, payload);
+          } else {
+            debug('topic ' + redisTopic + ' not saved 2');
+          }
+          return;
+        }
+      } catch (e) {
+        // not a json compare payloads
+        if (reply != payload) {
+          saveToRedis(redisTopic, payload);
+        } else {
+          debug('topic ' + redisTopic + ' not saved 1');
+        }
+        return;
+      }
+    });
+  } else {
+    saveToRedis(redisTopic, payload);
+  }
+}
+
+function saveToRedis(redisTopic, payload) {
+  var history = {
+    value : payload
+  };
+  if (program.timestamp == true) {
+    history.timestamp = new Date();
+  }
+  redisClient.lpush(program.redisprefix + ':' + redisTopic, JSON.stringify(history));
+  debug('lpush ' + program.redisprefix + ':' + redisTopic + ' : ' + JSON.stringify(history));
+}
